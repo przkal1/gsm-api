c775f032c56c91437766aef5aa1f61e64b6da018
Przemek <przkal1@gmail.com>
2024-08-22 08:11:03 +0200

    WIP

---
+++ gsm-api-server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('frontend'))
+
+
+app.get('/', function (req, res) {
+    res.sendFile('frontend/index.html');
+})
+
+
+app.listen(3002, () => {})
