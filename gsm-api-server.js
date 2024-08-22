aef4886495730b255eea406059d5a698f9a9553b
Przemek <przkal1@gmail.com>
2024-08-22 08:41:27 +0200

    WIP

--- gsm-api-server.js
+++ gsm-api-server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const validApiKeys = ["a", "b"];
+
+function apiKeyMiddleware(req, res, next) {
+    const apiKey = req.header('x-api-key'); // The client should send the key in this header
+    if (!apiKey || !validApiKeys.has(apiKey)) {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+    next();
+}
+
 const app = express()
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
@@ -10,9 +20,14 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('frontend'))
 
 
+app.use(apiKeyMiddleware);
+
 app.get('/', function (req, res) {
     res.sendFile('frontend/index.html');
 })
 
+app.get('/secure-endpoint', apiKeyMiddleware, (req, res) => {
+    res.json({ message: 'You have access to this secure endpoint!' });
+});
 
 app.listen(3003, () => {})
