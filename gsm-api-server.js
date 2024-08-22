791537d8f013093f97c3e87ef1aeea870e0e9206
Przemek <przkal1@gmail.com>
2024-08-22 15:07:57 +0200

    WIP

--- gsm-api-server.js
+++ gsm-api-server.js
@@ -61,7 +61,7 @@ app.get('/secure-endpoint', apiKeyMiddleware, (req, res) => {
 
 app.post('/send-sms', apiKeyMiddleware, (req, res) => {
 	if (!req.body.phoneNumber || !req.body.message){
-		return res.status(400);
+		res.status(400);
 	}
 	
     res.json({ msg: 'SENDING SMS: '+ req.body.phoneNumber + " " + req.body.message});
